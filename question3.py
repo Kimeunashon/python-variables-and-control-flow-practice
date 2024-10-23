92227b900afe7ddb99b66fc1d0354f53e5e37e9e
Nasho99 <kimeunashon@gmail.com>
2024-10-23 12:52:31 +0300

    Added solution for question 3:Grade Categorizer

---
+++ question3.py
@@ -0,0 +1,13 @@
+# question3.py: Grade Categorizer
+score = int(input("Enter a score (0-100): "))
+
+if 90 <= score <= 100:
+    print("Grade: A")
+elif 80 <= score <= 89:
+    print("Grade: B")
+elif 70 <= score <= 79:
+    print("Grade: C")
+elif 60 <= score <= 69:
+    print("Grade: D")
+else:
+    print("Grade: F")
